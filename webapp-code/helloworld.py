41643d5ff23057297c36aa946799bd9fcad39f23
ysharma1126 <ysharma1126@gmail.com>
2015-10-27 18:35:29 -0400

    fes

--- webapp-code/helloworld.py
+++ webapp-code/helloworld.py
@@ -14,12 +14,24 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+from google.appengine.api import users
+
 import webapp2
 
-class MainHandler(webapp2.RequestHandler):
+
+class MainPage(webapp2.RequestHandler):
+
     def get(self):
-        self.response.write('Hello world!')
+        # Checks for active Google account session
+        user = users.get_current_user()
+
+        if user:
+            self.response.headers['Content-Type'] = 'text/html; charset=utf-8'
+            self.response.write('Hello, ' + user.nickname())
+        else:
+            self.redirect(users.create_login_url(self.request.uri))
+
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainPage),
 ], debug=True)
